e8d34f1dea4cf735bcbc0f7c9e63650b3157f84e
Bart Skowron <bart.skowron@rollbar.com>
2021-02-16 20:10:15 +0100

    Make the example Python3 compatible

--- send_job.py
+++ send_job.py
@@ -1,8 +1,9 @@
+from __future__ import print_function
 from tasks import add
 
 def main():
     res = add.delay(4,4)
-    print res.get(timeout=1)
+    print(res.get(timeout=1))
 
 if __name__ == '__main__':
     main()
